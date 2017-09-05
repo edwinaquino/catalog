fc563c66084ca3ae4bc1fbbd1db7447624288aa5
edwinaquino <github@edwinaquino>
2017-09-05 11:55:35 -0700

    update .gitignore file to ignore some files

--- project.py
+++ project.py
@@ -257,17 +257,17 @@ def showLogin():
 # GConnect
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
-    # Validate state token
+    # Validate  token state
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    # Obtain authorization code, now compatible with Python3
+    # Obtain authorization code
     request.get_data()
     code = request.data.decode('utf-8')
 
     try:
-        # Upgrade the authorization code into a credentials object
+        # Upgrade the authorization code
         oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
@@ -277,23 +277,23 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Check that the access token is valid.
+    # Check if access token is valid.
     access_token = credentials.access_token
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
            % access_token)
-    # Submit request, parse response - Python3 compatible
+    # Submit request, parse response - (Python3 compatible)
     h = httplib2.Http()
     response = h.request(url, 'GET')[1]
     str_response = response.decode('utf-8')
     result = json.loads(str_response)
 
-    # If there was an error in the access token info, abort.
+    # ERROR: If there was an error in the access token info, abort.
     if result.get('error') is not None:
         response = make_response(json.dumps(result.get('error')), 500)
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Verify that the access token is used for the intended user.
+    # Verify that the access token is used for this user.
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
         response = make_response(
@@ -301,7 +301,7 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Verify that the access token is valid for this app.
+    # Verify access token is valid for this app.
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
@@ -316,11 +316,11 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Store the access token in the session for later use.
+    # Save access token.
     login_session['access_token'] = access_token
     login_session['gplus_id'] = gplus_id
 
-    # Get user info
+    # Gather user info
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
     params = {'access_token': access_token, 'alt': 'json'}
     answer = requests.get(userinfo_url, params=params)
@@ -331,7 +331,7 @@ def gconnect():
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
 
-    # see if user exists, if it doesn't make a new one
+    # check if user exists, if not, make a new user profile
     user_id = getUserID(login_session['email'])
     if not user_id:
         user_id = createUser(login_session)
@@ -345,13 +345,15 @@ def gconnect():
     output += login_session['picture']
     output += ' " class="user_profile_img"> '
     if not login_session['username']:
-        # in case user does not have a name in their google profile, show their email
+        # in case user does not have a name in their google profile,
+        #show their email
         login_session['username'] = login_session['email']
     flash("You're logged in as %s" % login_session['username'])
     return output
 
-# User Helper Functions
-
+#############################
+#Functions for User Helper
+#############################
 
 def createUser(login_session):
     newUser = User(name=login_session['username'], email=login_session[
